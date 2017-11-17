51eb5eee53923542b9e2b6931e052370f058c281
Philipp Schwarz <31252555+m4st3rP@users.noreply.github.com>
2017-11-17 01:42:39 +0100

    Update aCounter(289).asm

--- aCounter(289).asm
+++ aCounter(289).asm
@@ -20,11 +20,11 @@ main	PROC
 		LDR		r0,=count	;r0 ist der Zaehler
 		MOV		r0,#0		;Zaehler fängt bei 0 an
 		LDR		r1,=string	;Der String wird in r1 geladen	
-loop	LDRB			r2,[r1]		;Das aktuelle Zeichen des Strings wird in r2 geladen
+loop		LDRB		r2,[r1]		;Das aktuelle Zeichen des Strings wird in r2 geladen
 		CMP		r2,#"a"		;Das aktuelle Zeichen wird mit "a" verglichen
 		BNE		skip		;Ist das aktuelle Zeichen kein "a" wird der nächste Schrit übersprungen
 		ADD		r0,r0,#1	;Auf den Zähler wird 1 addiert
-skip	ADD			r1,r1,#1	;Die nächste Stelle des Strings wird in r1 ausgewählt
+skip		ADD		r1,r1,#1	;Die nächste Stelle des Strings wird in r1 ausgewählt
 		CMP		r2,#"0"		;Es wird geguckt ob das aktuelle Teichen 0 ist
 		BNE		loop		;Sollte dies nicht der Fall sein wird der Vorgang wiederholt
 		LDR		r3,=tot		;In r3 wird der reservierte Speicher geladen
